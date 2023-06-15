9cf48049bd9f80b73ef582dbaecfab61bd553816
youjin <w2117@e-mirim.hs.kr>
2023-06-15 20:07:16 +0900

    server

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const mysql = require('mysql2');
 const config = require('./config');
-const cors = require('cors');
 const session = require('express-session');
 // CORS 설정
 app.use(express.json());
@@ -34,6 +33,7 @@ app.post('/api/signup', (req, res) => {
     });
 });
 
+//로그인 
 app.post('/api/login', (req, res) => {
     console.log("login")
     const { email, password } = req.body;
@@ -45,26 +45,67 @@ app.post('/api/login', (req, res) => {
             res.status(500).json({ message: '서버 오류' });
             return;
         }
-
         if (results.length === 0) {
             // 사용자가 존재하지 않는 경우
             res.status(401).json({ message: '로그인 실패: 이메일 또는 닉네임 또는 비밀번호가 잘못되었습니다.' });
             return;
         }
-
         const user = results[0];
-
         if (user.pw !== password) {
             // 비밀번호가 일치하지 않는 경우
             res.status(401).json({ message: '로그인 실패: 이메일 또는 닉네임 또는 비밀번호가 잘못되었습니다.' });
             return;
         }
-
         // 로그인 성공
         req.session.user = user; // 세션에 사용자 정보 저장
+        console.log(user)
         res.json({ message: '로그인 성공', user });
     });
 });
+
+// 사용자 정보 조회 엔드포인트
+app.get('/api/user', (req, res) => {
+    const user = req.session.user;
+    if (!user) {
+      res.status(401).json({ message: '로그인이 필요합니다.' });
+      return;
+    }
+    res.json(user); // 사용자 정보를 응답으로 보냄
+  });
+
+// 로그아웃 처리
+app.post('/api/logout', (req, res) => {
+    req.session.destroy((err) => {
+        if (err) {
+            console.log(err);
+            res.status(500).json({ message: '로그아웃 실패' });
+        } else {
+            res.clearCookie('connect.sid'); // 쿠키 삭제
+            res.json({ message: '로그아웃 성공' });
+        }
+    });
+});
+
+// 현재 학습률 저장 
+// 회원가입 API 엔드포인트
+app.post('/api/update', (req, res) => {
+    // 클라이언트로부터 전달받은 사용자 정보
+    const { email, password, nickname } = req.body;
+
+    // TODO: 데이터베이스에 사용자 정보 저장 로직 구현
+    // 회원 정보를 데이터베이스에 저장
+    const query = 'INSERT INTO users (email, pw, nickname) VALUES (?, ?, ?)';
+    connection.query(query, [email, password, nickname], (error, results) => {
+        if (error) {
+            console.error('회원가입 중 오류 발생:', error);
+            res.status(500).json({ error: '회원가입에 실패했습니다.' });
+        }
+        console.log('회원가입 성공:', results);
+        res.json({ message: '회원가입이 성공적으로 완료되었습니다.' });
+    });
+});
+
+// 게임 구현 
 
 connection.connect((error) => {
     if (error) {
