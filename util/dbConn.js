83d8ac8429b9e0306433cb0d5f1f91d8b79b585c
koldin108902 <alex108902@naver.com>
2023-06-20 16:52:43 +0900

    Fix: promise 형태의 connection 리턴

--- util/dbConn.js
+++ util/dbConn.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql2');
-const config = require('./config');
+const config = require('../config');
 const connection = mysql.createConnection(config);
+connection.connect()
 
-module.exports = connection
+module.exports = connection.promise()
